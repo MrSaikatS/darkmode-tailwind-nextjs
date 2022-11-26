7a5303b4307378b075854aed8d1da008ce30a182
Saikat Sardar <saikatsardar5@gmail.com>
2022-11-26 22:01:08 +0530

    tailwindcss

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+const index = () => {
+	return (
+		<>
+			<div className=''>
+				<div className='text-5xl font-semibold p-4 dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-purple-400 dark:to-pink-600'>
+					Home Page
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default index;
